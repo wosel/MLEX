f3710283d4f963ecceacf2120c7cb65540eac78a
Jakub Hajic <j.hajic@gmail.com>
2015-02-18 12:27:29 +0100

    HW2: more structure in tests, more tests

--- hw02/testHW2.py
+++ hw02/testHW2.py
@@ -6,13 +6,17 @@ import front_times
 
 class TestHW2(unittest.TestCase):
     def testFrontTimes(self):
-        orig = 'Chocolate'
-        repeat = 3
-        expResult = 'ChoChoCho'
-        actResult = front_times.frontTimes(orig, repeat)
-        print '\n\nExpected: frontTimes(\'{orig}\', {repeat}) -> {expResult}'.format(orig=orig, repeat=repeat, expResult=expResult)
-        print 'Result: {actResult}'.format(actResult=actResult)
-        self.assertEqual(expResult, actResult)
+        testList = [
+            {'orig': 'Chocolate', 'repeat': 3, 'expResult': 'ChoChoCho'},
+            {'orig': 'Chocolate', 'repeat': 2, 'expResult': 'ChoCho'},
+            {'orig': 'xx', 'repeat': 3, 'expResult': 'xxxxxx'},
+        ]
+        for x in testList:
+            actResult = front_times.frontTimes(x['orig'], x['repeat'])
+            print '\n\nExpected: frontTimes(\'{orig}\', {repeat}) -> {expResult}'.format(orig=x['orig'], repeat=x['repeat'], expResult=x['expResult'])
+            print 'Result: {actResult}'.format(actResult=actResult)
+            self.assertEqual(x['expResult'], actResult)
+
 
 
 if __name__ == '__main__':
